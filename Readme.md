bd38b4e2b89dc96c048579986bd36d1863879204
zac <zgithub@hotmail.com>
2018-06-20 22:01:22 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# 概要
+https://qiita.com/cacarrot/items/f5e7511928302452b1dc
+https://gist.github.com/cacarrot/7bfa164511f1b461605c686d43446c22
---
+++ src/main/java/com/example/demo/ZacSpringbootTymeleafDomaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZacSpringbootTymeleafDomaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZacSpringbootTymeleafDomaExampleApplication.class, args);
+	}
+}
